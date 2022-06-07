056258d2de15714120fc5a4b58d51d38f0d5a7c6
yosef langer <yosef@doar.systems>
2022-06-07 18:19:27 +0300

    Update defaultComponentDecorator.jsx

--- src/decorators/defaultComponentDecorator.jsx
+++ src/decorators/defaultComponentDecorator.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 export default (decoratedHref: string, decoratedText: string, key: number): React.Node => {
   return (
-    <a href={decoratedHref} key={key}>
+    <a href={decoratedHref} target="_blank" rel="nofollow noreferrer external help" key={key}>
       {decoratedText}
     </a>
   );
